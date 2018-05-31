c01c022a8f7af6811a16732f193a6124a149acea
Antonella Bernobich Dean <aberdean@users.noreply.github.com>
2018-05-31 11:32:09 +0200

    Cache all pages

--- mws-restaurant-stage-1/sw.js
+++ mws-restaurant-stage-1/sw.js
@@ -33,7 +33,7 @@ self.addEventListener('install', (event) => {
  */
 self.addEventListener('fetch', (event) => {
 	event.respondWith(
-		caches.match(event.request).then((response) => {
+		caches.match(event.request, {ignoreSearch: true}).then((response) => {
 			return response || fetch(event.request);
 		})
 	);
